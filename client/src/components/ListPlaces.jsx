7270aa0e7fb891098d2827482dde0eb56c21785e
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-07 16:39:44 -0700

    fixed bug: when user goes to their profile and from there click 'My Maps', their id is not passed and code breaks

--- client/src/components/ListPlaces.jsx
+++ client/src/components/ListPlaces.jsx
@@ -13,6 +13,7 @@ import star4 from '../images/small_4@3x.png';
 import star4_half from '../images/small_4_half@3x.png';
 import star5 from '../images/small_5@3x.png';
 import './ListPlaces.css';
+import AddToMapButton from './AddToMapButton';
 
 export class ListPlaces extends Component {
 	state = {
@@ -70,83 +71,110 @@ export class ListPlaces extends Component {
 		const place = this.props.place;
 		const index = this.props.index;
 
+		const starsNum = place.stars.toString();
+		const starMap = {
+			'0': star0,
+			'1': star1,
+			'1.5': star1_half,
+			'2': star2,
+			'2.5': star2_half,
+			'3': star3,
+			'3.5': star3_half,
+			'4': star4,
+			'4.5': star4_half,
+			'5': star5
+		};
+
 		let stars;
-		if (place.stars === 0) {
-			stars = <img src={star0} alt='0 star' width='' />
-		} else if (place.stars === 1) {
-			stars = <img src={star1} alt='1 star' />
-		} else if (place.stars === 1.5) {
-			stars = <img src={star1_half} alt='1.5 star' />
-		} else if (place.stars === 2) {
-			stars = <img src={star2} alt='2 star' />
-		} else if (place.stars === 2.5) {
-			stars = <img src={star2_half} alt='2.5 star' />
-		} else if (place.stars === 3) {
-			stars = <img src={star3} alt='3 star' />
-		} else if (place.stars === 3.5) {
-			stars = <img src={star3_half} alt='3.5 star' />
-		} else if (place.stars === 4) {
-			stars = <img src={star4} alt='4 star' />
-		} else if (place.stars === 4.5) {
-			stars = <img src={star4_half} alt='4.5 star' />
-		} else {
-			stars = <img src={star5} alt='5 star' />
+		for(let key in starMap) {
+			if(key === starsNum) {
+				stars = <img src={starMap[key]} alt={`${place.stars} stars`} />;	
+			}
 		}
 
 		return (
-			<tr>
-				<td>			
-					<div className="image">
-						<img src={place.image} alt={place.name} width='200px' height='200px' />
-					</div>							
-				</td>
-				<td>
-					<div className="description">
-						<p>{place.name}</p>
-						<p>{place.address}</p>
-						<p>
-							{stars}
-							<a href={place.yelpLink}><img src={logo} alt={logo} /></a>
-						</p>
-						<p><i>{place.reviewCount} Reviews</i></p>
-					</div>
-				</td>
-				<td>
-					<Button
-						bsStyle="primary" bsSize="large"
-						id='addbtn' data-id={index} onClick={this.handleShow}
-					>
-						Add to Map
-					</Button>
-					<Modal bsSize='small' backdrop='static' keyboard={false} show={this.state.show} >
-						<Modal.Header id='modal-header'>
-							<Modal.Title id='modal-title'>Map Category</Modal.Title>
-						</Modal.Header>
-						<Modal.Body id='modal-body'>
-							<h4>Select a category:</h4>
-							<form>
-								<input type='radio' name='category' value='museum' /> Museum<br />
-								<input type='radio' name='category' value='restaurant' /> Restaurant<br />
-								<input type='radio' name='category' value='dessert' /> Dessert<br />
-								<input type='radio' name='category' value='bar' /> Bar<br />
-								<input type='radio' name='category' value='hike' /> Hike<br />
-								<input type='radio' name='category' value='photo-op' /> Photo-op<br />
-								<input type='radio' name='category' value='romantic' /> Romantic<br />
-							</form>
-						</Modal.Body>
-						<Modal.Footer>
-							<Button id='savebtn' onClick={() => {
-								this.handleClose();
-								//saving the index for each place until you click on it
-								this.saveMapInfo(index);
+			// <div className='place-wrapper'>
+				<tr>
+					<td>
+						<div className="image">
+							<img src={place.image} alt={place.name} width='200px' height='200px' />
+						</div>
+					</td>
+					<td>
+						<div className="description">
+							<p>{place.name}</p>
+							<p>{place.address}</p>
+							<p>
+								{stars}
+								<a href={place.yelpLink}><img src={logo} alt={logo} /></a>
+							</p>
+							<p><i>{place.reviewCount} Reviews</i></p>
+						</div>
+					</td>
+					<td>
+						{/* <AddToMapButton handleShow={this.handleShow}/> */}
+						<Button
+							bsStyle="primary" bsSize="large"
+							id='addbtn' data-id={index} onClick={this.handleShow}
+						>
+							Add to Map
+						</Button>
+
+						<Modal bsSize='small' backdrop='static' keyboard={false} show={this.state.show} >
+							<Modal.Header id='modal-header'>
+								<Modal.Title id='modal-title'>Map Category</Modal.Title>
+							</Modal.Header>
+							<Modal.Body id='modal-body'>
+								<h4>Select a category:</h4>
+								<form>
+									<input type='radio' name='category' value='museum' /> Museum<br />
+									<input type='radio' name='category' value='restaurant' /> Restaurant<br />
+									<input type='radio' name='category' value='dessert' /> Dessert<br />
+									<input type='radio' name='category' value='cafe' /> Cafe<br />
+									<input type='radio' name='category' value='bar' /> Bar<br />
+									<input type='radio' name='category' value='hike' /> Hike<br />
+									<input type='radio' name='category' value='photo-op' /> Photo-op<br />
+									<input type='radio' name='category' value='romantic' /> Romantic<br />
+								</form>
+							</Modal.Body>
+							<Modal.Footer>
+								<Button id='savebtn' onClick={() => {
+									this.handleClose();
+									//saving the index for each place until you click on it
+									this.saveMapInfo(index);
 								}}
-							>
-								Save
+								>
+									Save
 							</Button>
-						</Modal.Footer>
-					</Modal>
-				</td>
-			</tr>
+							</Modal.Footer>
+						</Modal>
+					</td>
+				</tr>
+			// </div>
 		);
 	}
 }
+
+//new way of writing classes; don't get passed in the props/state of React Component (to save space)
+//({title}) -> same as writing {this.props.title}
+// const Title = ({ title, className }) => (
+// 	<h1 className={className}>{title}</h1>
+// );
+
+// <Title title="hey" />
+
+// class Title extends React.Component {
+// 	render () {
+// 		const title = props.title;
+// 		return <h1>{title}</h1>;
+// 	}
+// }
+
+// const AddToMapButton = () => (
+// 	<Button
+// 		bsStyle="primary" bsSize="large"
+// 		id='addbtn' data-id={index} onClick={this.handleShow}
+// 	>
+// 		Add to Map
+// 	</Button>
+// )
