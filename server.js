01a8afe2744bd88343efed4f5d774060b109a9f5
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-21 16:45:41 -0700

    added yarn build to package.json

--- server.js
+++ server.js
@@ -134,5 +134,5 @@ app.post('/userData', (req, res) => {
 })
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, host, () => console.log(`Listening on port ${port}`));
