626dc3e5354e3334a17bafa4a37c4d3bc50f5bbe
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-21 16:21:53 -0700

    added engines in package.json

--- server.js
+++ server.js
@@ -134,5 +134,5 @@ app.post('/userData', (req, res) => {
 })
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, host, () => console.log(`Listening on port ${port}`));
