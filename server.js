1af76d477440a4e345c36e21d89a436482172079
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-22 10:29:13 -0700

    changed server port

--- server.js
+++ server.js
@@ -136,5 +136,5 @@ app.post('/userData', (req, res) => {
 	})
 })
 
-const port = process.env.PORT || 8000;
+var port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Listening on port ${port}`));
