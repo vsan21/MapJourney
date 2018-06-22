7940e16d99764649b66bf1e86f1febc7331feaf0
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-22 10:20:00 -0700

    updated server port

--- server.js
+++ server.js
@@ -136,6 +136,5 @@ app.post('/userData', (req, res) => {
 	})
 })
 
-const host = '0.0.0.0';
 const port = process.env.PORT || 8000;
-app.listen(port, host, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
