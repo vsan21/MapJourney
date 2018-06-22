0bb371d2b16c389545df0b1e3037a8eebd2a6355
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-22 12:12:04 -0700

    created new db config

--- database/connection.js
+++ database/connection.js
@@ -7,18 +7,18 @@ const MYSQL_KEY = process.env.MYSQL_KEY;
 // create MySQL connection
 let dbConfig;
 
-// if(process.env.PORT === undefined) {
-//   dbConfig = {
-//     host: 'localhost',
-//     user: 'root',
-//     password: `${MYSQL_KEY}`,
-//     database: 'mapjourneytest'
-//   }
-// } else {
-//     dbConfig = process.env.CLEARDB_DATABASE_URL;
-// }
+if(process.env.PORT === undefined) {
+  dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: `${MYSQL_KEY}`,
+    database: 'mapjourneytest'
+  }
+} else {
+    dbConfig = process.env.DATABASE_URL;
+}
 
-const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+var connection = mysql.createConnection(dbConfig)
 
 connection.connect((err) => {
   if(err) throw err;
