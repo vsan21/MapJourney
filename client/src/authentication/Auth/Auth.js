0001f6f385d4ed4c1dda43a46cc3f3b620d52c55
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-06-24 00:37:59 -0700

    production auth0 vars are now working

--- client/src/authentication/Auth/Auth.js
+++ client/src/authentication/Auth/Auth.js
@@ -19,9 +19,6 @@ if(process.env.NODE_ENV === 'development') {
 	audience = `https://${process.env.REACT_APP_AUTH0_PROD_DOMAIN}/userinfo`;
 }
 
-console.log(`auth: ${process.env.NODE_ENV}`);
-console.log(`test: ${domain} ${clientID} ${redirectUri} ${audience}`);
-
 export default class Auth {
 	auth0 = new auth0.WebAuth({
 		domain: domain,
